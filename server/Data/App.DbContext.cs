ab36de9ec2b05f0da9ad14dce49964afc0f9969c
Devora Wexler <devora.video@gmail.com>
2025-12-12 14:24:38 +0200

    new installtions

---
+++ server/Data/App.DbContext.cs
@@ -0,0 +1,14 @@
+// using Microsoft.EntityFrameworkCore;
+// using server.Models;
+
+// namespace server.Data;
+
+// public class AppDbContext : DbContext
+// {
+//     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+//     {
+//     }
+
+//     public DbSet<DonorModel> Donors { get; set; }
+//     public DbSet<GiftModel> Gifts { get; set; }
+// }
