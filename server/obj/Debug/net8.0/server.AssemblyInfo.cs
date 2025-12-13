5f30a805292d3490da901426bf4a26a61df9a181
Devora Wexler <devora.video@gmail.com>
2025-12-13 20:16:50 +0200

    adding new folders

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93920f52476a91193b3dc265d59b3d9a638b5c9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4bea6ad74bb0d8a533f3e19ab8b0de0b9ed774")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
