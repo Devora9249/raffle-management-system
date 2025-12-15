44b671df452b2595fc9f1c1d976577e90c006fca
Devora Wexler <devora.video@gmail.com>
2025-12-15 21:39:04 +0200

    change

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4726c2c6faaffeeb178f337b4688fb086886fd0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5773990cd58f816bc0b4c06f6485987952f9d68d")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
