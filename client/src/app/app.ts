a92fac2e4e77ff2c968ca05e79771728ab9c89ef
YaeliP <porat4241@gmail.com>
2025-12-29 22:05:39 +0200

    start register

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Register } from './features/auth/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,Register],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
