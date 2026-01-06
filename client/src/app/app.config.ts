01db56542174b4f87a0c45e9434e43fb922ab686
Devora Wexler <devora.video@gmail.com>
2026-01-07 00:01:22 +0200

    add Auth and httpError Interceptors

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -7,54 +7,32 @@ import { catchError, throwError, Observable } from 'rxjs';
 
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
 import mainDesign from './theme/mainDesign';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AuthInterceptor } from './core/Interceptors/authInterceptor';
+import { HttpErrorInterceptor } from './core/Interceptors/HTTPErrorInterceptor';
+import { GlobalErrorHandler } from './core/Errors/globalErrorHendler';
 
 
-// ----------------------
-// Global Error Handler
-// ----------------------
-@Injectable({ providedIn: 'root' })
-export class GlobalErrorHandler implements ErrorHandler {
-  handleError(error: any): void {
-    console.error('Global Error:', error);
-  }
-}
 
-// ----------------------
-// HTTP Error Interceptor
-// ----------------------
-@Injectable()
-export class HttpErrorInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(
-      catchError((err: HttpErrorResponse) => {
-        console.error('HTTP Error:', {
-          status: err.status,
-          statusText: err.statusText,
-          url: err.url,
-          error: err.error
-        });
-        return throwError(() => err);
-      })
-    );
-  }
-}
 
-// ----------------------
+
+
 // Application Config
-// ----------------------
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     // Global error handling
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    //error handler
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
 
     // HTTP interceptor
     { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
+    //auth Interceptor
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
 
     // Router
     provideRouter(routes),
