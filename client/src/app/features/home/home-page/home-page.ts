a58a6c0592c4f3f7a760c5c175b5d03ebb7b09d8
Devora Wexler <devora.video@gmail.com>
2026-01-28 15:22:43 +0200

    home page adding

--- client/src/app/features/home/home-page/home-page.ts
+++ client/src/app/features/home/home-page/home-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ButtonModule, RouterModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
