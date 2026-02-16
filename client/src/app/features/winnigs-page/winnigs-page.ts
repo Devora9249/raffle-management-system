935d458e81aa9e14002820747cafc554aa9efec9
YaeliP <porat4241@gmail.com>
2026-02-16 16:16:53 +0200

    winnings page show winners&gifts

--- client/src/app/features/winnigs-page/winnigs-page.ts
+++ client/src/app/features/winnigs-page/winnigs-page.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WinningService } from '../../core/services/winning-service';
+import { WinningResponseDto } from '../../core/models/winning-model';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-winnigs-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CardModule, TagModule, ProgressSpinnerModule],
   templateUrl: './winnigs-page.html',
   styleUrl: './winnigs-page.scss',
 })
-export class WinnigsPage {
+export class WinnigsPage implements OnInit {
+  winnings: WinningResponseDto[] = [];
+  loading: boolean = false;
 
+  constructor(private winningService: WinningService) {}
+
+  ngOnInit() {
+    this.loadWinnings();
+  }
+
+  loadWinnings() {
+    this.loading = true;
+    this.winningService.getAll().subscribe({
+      next: (res) => {
+        this.winnings = res;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error fetching winnings:', err);
+        this.loading = false;
+      }
+    });
+  }
 }
